b9fc703a8ed497e0e07ed95a451e1ce737f4f531
Imagion <algernoncowperwood@gmail.com>
2023-12-15 16:47:02 +0300

    update

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -2,6 +2,13 @@ export default function ContactPage() {
   return (
     <main>
       <h1 className="text-xl md:text-2xl">Contact</h1>
+      <div className="mt-6 grid grid-cols-1">
+        <ul>
+          <li>List</li>
+          <li>List</li>
+          <li>List</li>
+        </ul>
+      </div>
     </main>
   );
 }
